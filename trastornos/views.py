2a62b29dbd75530b3eb8c71eeb17e11c54438ff3
IndiraHanccoZaravia <64119840+IndiraHanccoZaravia@users.noreply.github.com>
2021-06-29 18:04:41 -0500

    mejoras

--- trastornos/views.py
+++ trastornos/views.py
@@ -8,14 +8,34 @@ def inicio(request):
 	print(queryset)
 	if(queryset=="nosotros"):
 		return render(request, 'nosotros.html')
-	if(queryset=="transtornos"):
+	if(queryset=="trastornos"):
 		return render(request, 'clasificacion.html')
 	return render(request, 'inicio.html')
+
+
 def nosotros(request):
+	queryset= request.GET.get("Buscar")
+	print(queryset)
+	if(queryset=="nosotros"):
+		return render(request, 'nosotros.html')
+	if(queryset=="trastornos"):
+		return render(request, 'clasificacion.html')
 	return render(request, 'nosotros.html')
 
 def suscripcion(request):
+	queryset= request.GET.get("Buscar")
+	print(queryset)
+	if(queryset=="nosotros"):
+		return render(request, 'nosotros.html')
+	if(queryset=="trastornos"):
+		return render(request, 'clasificacion.html')
 	return render(request, 'suscripcion.html')
 
 def clasificacion(request):
+	queryset= request.GET.get("Buscar")
+	print(queryset)
+	if(queryset=="nosotros"):
+		return render(request, 'nosotros.html')
+	if(queryset=="trastornos"):
+		return render(request, 'clasificacion.html')
 	return render(request, 'clasificacion.html')
